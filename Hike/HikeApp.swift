7222c01860432f3b1225c4304595486bd9cf1f48
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2024-07-05 10:04:04 +0700

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Nguyen Quang Minh on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
